ca084d479447f4ce1d44cfd0629c7423ecbe392d
Giorgio Riccardi <griccardi@my.bcit.ca>
2019-08-13 09:23:48 -0700

    add Header component

--- bimsearch/src/App.js
+++ bimsearch/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className='App'>
       <h1>App</h1>
+      <header className='App-header'>
+        <Header />
+      </header>
     </div>
   );
 }
--- bimsearch/src/components/Header.js
+++ bimsearch/src/components/Header.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 
 export class Header extends Component {
   render() {
-    return <div />;
+    const logo =
+      'https://www.seatoskywebsolutions.ca/wordpress/wp-content/uploads/2019/04/SSWS_logo_blue.png';
+    return (
+      <div>
+        <p>
+          <img src={logo} className='App-logo' alt='logo' />
+        </p>
+        <h1 className='App-title'>
+          SSWS - BIM Business Search Form with React SPA w/ headless WP
+        </h1>
+      </div>
+    );
   }
 }
 

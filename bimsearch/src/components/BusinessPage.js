4c44616fb1573bc8b02e0f2ca7a5e6703508a829
Giorgio Riccardi <griccardi@my.bcit.ca>
2019-09-23 19:39:55 -0700

    set single business state

--- bimsearch/src/components/BusinessPage.js
+++ bimsearch/src/components/BusinessPage.js
@@ -1,8 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export class BusinessPage extends Component {
+  state = {
+    busines: {},
+    isLoaded: false
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        `http://bim-business-search.local/?rest_route=/bim-businesses/v1/posts/${this.props.match.params.id}`
+      )
+      .then(res =>
+        this.setState({
+          business: res.data,
+          isLoaded: true
+        })
+      )
+      .catch(err => console.log(err));
+  }
+
   render() {
-    return <div>code here</div>;
+    const { business, isLoaded } = this.state;
+    if (isLoaded) {
+      return (
+        <Fragment>
+          <Link to='/'>Home</Link>
+          <br />
+          <h2>{business.title}</h2>
+          <h2>{business.acf.licence_number}</h2>
+        </Fragment>
+      );
+    }
+    return <h3>Loading...</h3>;
   }
 }
 

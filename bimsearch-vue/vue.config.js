45e4cc977bf274c334ff26df9a95f6b5a0ea68b7
Giorgio Riccardi <griccardi@my.bcit.ca>
2019-10-01 14:11:04 -0700

    change vue config to point to bimsearch-vue folder

--- bimsearch-vue/vue.config.js
+++ bimsearch-vue/vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === 'production' ? '/vue-search-ui-demo/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/bimsearch-vue/' : '/'
 };

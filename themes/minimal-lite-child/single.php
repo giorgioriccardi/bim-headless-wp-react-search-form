dd7ac8a125818f4f59aef649fba0b707035efe9d
Giorgio Riccardi <griccardi@my.bcit.ca>
2019-10-15 12:06:24 -0700

    add naics_code link output code in single.php

--- themes/minimal-lite-child/single.php
+++ themes/minimal-lite-child/single.php
@@ -59,9 +59,30 @@ get_header();?>
 $args = array('number' => '1');
 $terms = get_terms('naics_code', $args);
 foreach ($terms as $term) {
-    echo $term->name;
+    $term_link = get_term_link($term);
+    // echo $term->name;
+    echo '<a href="' . esc_url($term_link) . '">' . $term->name . '</a>';
 }
 ?>
+
+<!-- <em>
+SSWS naics_code loop with links: -->
+<?php
+// $terms = get_terms('naics_code');
+// echo '<ul>';
+// foreach ($terms as $term) {
+//     // The $term is an object, so we don't need to specify the $taxonomy.
+//     $term_link = get_term_link($term);
+//     // If there was an error, continue to the next term.
+//     if (is_wp_error($term_link)) {
+//         continue;
+//     }
+//     // We successfully got a link. Print it out.
+//     echo '<li><a href="' . esc_url($term_link) . '">' . $term->name . '</a></li>';
+// }
+// echo '</ul>';
+?>
+<!-- </em> -->
                 </p>
             </div>
 
